d23eb2d018db4645874c028cbbfcbc4f889f9154
Craig Lam <cl@kpv-lab.co.uk>
2021-12-24 17:30:09 +0000

    feat: add xmas text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { beforeUpdate, onMount } from 'svelte';
+	import SettingsIcon from '../settings.svg'
 
 	let font = 'monospace';
 	let fontSize = 16;
-	let textColor;
 	let windowHeight;
 	let windowWidth;
 	let interval = 66;
@@ -12,7 +12,70 @@
 	let columns;
 	let drops = [];
 
+	// controls
+	let colorSchemeIdx = 0;
+
+	interface ColorScheme {
+		bgColor: string;
+		colors: string[];
+	}
+
+	const xmasColors: ColorScheme = {
+		bgColor: 'rgba(0, 0, 0, 0.05)',
+		colors: ['red', 'green']
+	};
+	const matrix: ColorScheme = {
+		bgColor: 'rgba(0, 0, 0, 0.05)',
+		colors: ['#03A062']
+	};
+	const colorSchemes: ColorScheme[] = [xmasColors, matrix];
 	const chars = ['M', 'E', 'R', 'R', 'Y', 'C', 'H', 'R', 'I', 'S', 'T', 'M', 'A', 'S'];
+	const matrixChars = [
+		'ﾊ',
+		'ﾐ',
+		'ﾋ',
+		'ｰ',
+		'ｳ',
+		'ｼ',
+		'ﾅ',
+		'ﾓ',
+		'ﾆ',
+		'ｻ',
+		'ﾜ',
+		'ﾂ',
+		'ｵ',
+		'ﾘ',
+		'ｱ',
+		'ﾎ',
+		'ﾃ',
+		'ﾏ',
+		'ｹ',
+		'ﾒ',
+		'ｴ',
+		'ｶ',
+		'ｷ',
+		'ﾑ',
+		'ﾕ',
+		'ﾗ',
+		'ｾ',
+		'ﾈ',
+		'ｽ',
+		'ﾀ',
+		'ﾇ',
+		'ﾍ',
+		'0',
+		'1',
+		'2',
+		'3',
+		'4',
+		'5',
+		'7',
+		'8',
+		'9',
+		'Z',
+		'|',
+		'/'
+	];
 
 	beforeUpdate(() => {
 		if (c) {
@@ -20,16 +83,17 @@
 			c.width = windowWidth;
 
 			columns = c.width / fontSize;
-      initialiseDrops()
-      
+			initialiseDrops();
 		}
 	});
 
-  const initialiseDrops = () => {
-    for (let x = 0; x < columns; x++) {
+	//an array of drops - one per column
+	//1 = y co-ordinate of the drop(same for every drop initially)
+	const initialiseDrops = () => {
+		for (let x = 0; x < columns; x++) {
 			drops[x] = 1;
 		}
-  }
+	};
 	onMount(() => {
 		c = <HTMLCanvasElement>document.getElementById('c');
 		ctx = c.getContext('2d');
@@ -40,27 +104,40 @@
 
 		columns = c.width / fontSize; //number of columns for the rain
 
-		//an array of drops - one per column
-		//1 = y co-ordinate of the drop(same for every drop initially)
-		initialiseDrops()
+		initialiseDrops();
 
-    if(c) {
-    setInterval(draw, interval);
-  }
+		if (c) {
+			setInterval(draw, interval);
+		}
 	});
 
 	//drawing the characters
 	function draw() {
-
-		ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+		ctx.fillStyle = colorSchemes[colorSchemeIdx].bgColor;
 		ctx.fillRect(0, 0, c.width, c.height);
-		ctx.fillStyle = 'red';
-
+		if (colorSchemes[colorSchemeIdx].colors.length > 1) {
+			ctx.fillStyle =
+				colorSchemes[colorSchemeIdx].colors[Math.floor(Math.random() * colorSchemes.length)];
+		} else {
+			ctx.fillStyle = colorSchemes[colorSchemeIdx].colors[0];
+		}
 		ctx.font = fontSize + `px ${font}`;
-		//looping over drops
+
+		let charIdx = 0;
 		for (let i = 0; i < drops.length; i++) {
-			//a random char
-			const text = chars[Math.floor(Math.random() * chars.length)];
+			let text = '';
+			if (colorSchemeIdx === 0) {
+				if (charIdx > chars.length - 1) {
+					charIdx = 0;
+				}
+				if (Math.random() < 0.85) {
+					text = chars[charIdx];
+				}
+				charIdx++;
+			} else {
+				text = matrixChars[Math.floor(Math.random() * matrixChars.length)];
+			}
+
 			ctx.fillText(text, i * fontSize, drops[i] * fontSize);
 
 			//sending the drop back to the top randomly after it has crossed the screen
@@ -71,25 +148,42 @@
 			drops[i]++;
 		}
 	}
-
-
 </script>
 
-<svelte:window bind:innerHeight={windowHeight} bind:innerWidth={windowWidth} />
+<svelte:window 
+	bind:innerHeight={windowHeight}
+	bind:innerWidth={windowWidth}
+/>
 
-<body>
+<body style={`background: ${colorSchemes[colorSchemeIdx].bgColor}`}>
+	<div class="icon">
+		<SettingsIcon />
+	</div>
 	<canvas id="c" />
 </body>
 
 <style>
-	/*basic reset*/
 	* {
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
 	}
+
 	body {
-		background: black;
+		position: relative;
+	}
+
+	.icon {
+		position: absolute;
+		top: 1rem;
+		right: 1rem;
+		height: 2rem;
+		width: 2rem;
+		border-radius: 50%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: aliceblue;	
 	}
 	canvas {
 		display: block;
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-
+import svg from '@poppanator/sveltekit-svg'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -9,6 +9,10 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
+		vite: {
+      // Options are optional
+      plugins: [svg()]
+    },
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
